73dbe4dfdf478583852c1f58c04269fb2daf7f08
Lachlan Donald <lachlan@ljd.cc>
2014-08-08 19:24:10 +1000

    MapStore is now safe for concurrent access

--- store.go
+++ store.go
@@ -1,24 +1,30 @@
 package httpcache
 
+import "sync"
+
 type Store interface {
 	Get(key string) (*Resource, bool)
 	Set(key string, r *Resource)
 }
 
-// BUG(lox): Needs a mutex for concurrency
 type MapStore struct {
+	mutex     sync.RWMutex
 	resources map[string]*Resource
 }
 
 func NewMapStore() *MapStore {
-	return &MapStore{map[string]*Resource{}}
+	return &MapStore{resources: map[string]*Resource{}}
 }
 
 func (m *MapStore) Get(k string) (*Resource, bool) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
 	r, ok := m.resources[k]
 	return r, ok
 }
 
 func (m *MapStore) Set(k string, r *Resource) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.resources[k] = r
 }
