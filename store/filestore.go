0e70c94326b88435f6a092ea95b3ad4c2e152ad9
Lachlan Donald <lachlan@ljd.cc>
2014-09-02 17:06:53 +1000

    Rolled back use of diskv in favor of simple implementation
    Our implementation results in a seekable reader, which makes it possible
    to serve range requests.

--- store/filestore.go
+++ store/filestore.go
@@ -4,53 +4,87 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
-
-	"github.com/peterbourgon/diskv"
+	"path/filepath"
+	"sync"
 )
 
 type FileStore struct {
-	d *diskv.Diskv
+	sync.RWMutex
+	dir string
 }
 
 func NewFileStore(dir string, maxSize uint64) (*FileStore, error) {
-	flatTransform := func(s string) []string {
-		return []string{}
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return nil, err
 	}
-
-	d := diskv.New(diskv.Options{
-		BasePath:     dir,
-		Transform:    flatTransform,
-		CacheSizeMax: maxSize,
-	})
-
-	return &FileStore{d}, nil
+	return &FileStore{dir: dir}, nil
 }
 
 func (fs *FileStore) Has(key string) bool {
-	return fs.d.Has(hashKey(key))
+	fs.RLock()
+	defer fs.RUnlock()
+
+	if _, err := os.Stat(fs.path(key)); err == nil {
+		return true
+	}
+
+	return false
 }
 
 func (fs *FileStore) Delete(key string) error {
-	return fs.d.Erase(hashKey(key))
+	fs.Lock()
+	defer fs.Unlock()
+
+	os.Remove(fs.path(key))
+	return nil
 }
 
 func (fs *FileStore) Read(key string) (io.ReadCloser, error) {
-	r, err := fs.d.ReadStream(hashKey(key), false)
+	fs.RLock()
+	defer fs.RUnlock()
 
+	f, err := os.Open(fs.path(key))
 	if os.IsNotExist(err) {
 		return nil, ErrNotExists
+	} else if err != nil {
+		return nil, err
 	}
 
-	return r, err
+	return f, nil
 }
 
 func (fs *FileStore) WriteFrom(key string, r io.Reader) error {
-	return fs.d.WriteStream(hashKey(key), r, false)
+	fs.RLock()
+	defer fs.RUnlock()
+
+	tempFile, err := ioutil.TempFile(fs.dir, "tmp")
+	if err != nil {
+		return err
+	}
+
+	defer os.Remove(tempFile.Name())
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, r)
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(fs.path(key)); err == nil {
+		os.Remove(fs.path(key))
+	}
+
+	if err = os.Link(tempFile.Name(), fs.path(key)); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func hashKey(key string) string {
+func (fs *FileStore) path(key string) string {
 	h := md5.New()
 	io.WriteString(h, key)
-	return fmt.Sprintf("%x", h.Sum(nil))
+	return filepath.Join(fs.dir, fmt.Sprintf("%x", h.Sum(nil)))
 }
